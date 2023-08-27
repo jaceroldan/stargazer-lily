7caf6cb43553ec8ce8e92336291b32222890d055
Jace Roldan <jaceroldan16@gmail.com>
2023-08-27 22:18:03 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+anime({
+    targets: '.css-selector-demo  .el',
+    left: '240px',
+    backgroundColor: '#FFF',
+    borderRadius: ['0%', '50%'],
+    easing: 'easeInOutQuad',
+    transition: '1s',
+});
